07aee6be0edc74e77fbdba6c3edf85faeac0f873
AIDEN <76594690+aiden3493@users.noreply.github.com>
2022-02-06 00:13:07 +0900

    commit

--- components/TeahcerToStudentLetter.tsx
+++ components/TeahcerToStudentLetter.tsx
@@ -1,6 +1,20 @@
+import { json } from "node:stream/consumers";
+import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import styled from "../styles/TeacherToStudentCSS.module.css";
 
 function Letter() {
+  const [filesArray, setFilesArray] = useState<object[]>([]);
+  const onDrop = useCallback((acceptedFiles) => {
+    setFilesArray((oldArray) => [...oldArray, acceptedFiles]);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive, isDragAccept } =
+    useDropzone({
+      onDrop,
+      multiple: true,
+    });
+
   return (
     <div>
       <form className={styled.writeLetterForm}>
@@ -16,8 +30,31 @@ function Letter() {
 
         {/* photos */}
         <h1>test</h1>
-        <p>이곳에 원하는 사진 파일을 업로드 해주세요</p>
+        <div
+          {...getRootProps()}
+          className={styled.dragAndDropZoneOut}
+          style={
+            filesArray.length === 0
+              ? { height: "100px" }
+              : { height: `${110 + 100 * filesArray.length}px` }
+          }>
+          <div
+            className={styled.dragAndDropZoneIn}
+            style={isDragAccept === true ? { borderColor: "gray" } : {}}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p>여기에 파일을 드롭해 주세요</p>
+            ) : (
+              <p>클릭 또는 드래그 앤 드롭으로 파일을 업로드해주세요</p>
+            )}
+          </div>
+        </div>
       </form>
+      <h1>여백</h1>
+      <h1>여백</h1>
+      <h1>여백</h1>
+      <h1>여백</h1>
+      <h1>여백</h1>
     </div>
   );
 }
